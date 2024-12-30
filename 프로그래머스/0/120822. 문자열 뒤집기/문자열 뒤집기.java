6aaad7ca57ef7bdc4c3d24b09d7a989d8ae90d55
namgung2223 <110527875+namgung2223@users.noreply.github.com>
2024-12-30 13:08:15 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 79.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<my_string.length(); i++){
+            sb.append(my_string.charAt(my_string.length()-1-i));
+        }
+        answer = sb.toString();
+        return answer;
+    }
+}
